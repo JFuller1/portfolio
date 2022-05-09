608243a7116a01446cce69dabdd5854c0a7c66f4
Jaeden Fuller <fuller.jc@outlook.com>
2022-05-09 18:53:51 -0500

    Final touches on index page

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,10 +4,8 @@ const header = document.querySelector('header');
 const name = document.querySelector('.name');
 const description = document.querySelector('.description');
 const body = document.querySelector('body');
-
-body.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
+const down = document.querySelector('.down');
+const about = document.querySelector('.about')
 
 header.addEventListener('animationend', function() {
     header.style.transform = 'none';
@@ -21,3 +19,7 @@ description.addEventListener('animationend', function() {
     description.style.transform = 'none';
     body.style.overflow = 'scroll';
 });
+
+down.addEventListener('click', function() {
+    about.scrollIntoView(false);
+})
